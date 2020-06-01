4a3ba5fdd90ff2377a05d3d96dc9e5b5c82ad631
lusiferjr <31337652+lusiferjr@users.noreply.github.com>
2020-06-01 13:30:30 +0530

    upload api

--- server/machi.py
+++ server/machi.py
@@ -1,8 +1,10 @@
-import fuckcsv
+# import fuckcsv
 import flask
 from flask import request, jsonify, request
-
+import os
+from werkzeug.utils import secure_filename
 app = flask.Flask(__name__)
+
 app.config["DEBUG"] = True
 
 
@@ -26,16 +28,53 @@ def csv_plotdouble():
         graphType = details['graphType']
         col1 = details['col1']
         col2 = details['col2']
-        obj.plot(graphType,col1,col2)
+        obj.plot(graphType, col1, col2)
 
-@app.route('/csv/plotGraph/single',methods=['Post'])
+
+@app.route('/csv/plotGraph/single', methods=['Post'])
 def csv_plotsingle():
-    obj=fuckcsv.Csv("train.csv")
+    obj = fuckcsv.Csv("train.csv")
     if request.method == "POST":
         details = request.form
         graphType = details['graphType']
         col1 = details['col1']
         col2 = details['col2']
-        obj.plot(graphType,col1,col2)
-def
+        obj.plot(graphType, col1, col2)
+
+
+def allowed_file(filename):
+    ALLOWED_EXTENSIONS = {'csv'}
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/upload', methods=['post'])
+def upload_file():
+    response = {"error": "true",
+                "message": "message",
+                "info": "info"
+                }
+    UPLOAD_FOLDER = r'X:\BOT\server\uploads'
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+    if 'file' not in request.files:
+        response["error"] = "true"
+        response["message"] = "file is missing"
+        response["info"] = "no file found"
+        return jsonify(response)
+    file = request.files['file']
+    if file.filename == '':
+        response["error"] = "true"
+        response["message"] = "file is missing"
+        response["info"] = "no file found"
+        return jsonify(response)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        response["error"] = "false"
+        response["message"] = "file is recived"
+        response["info"] = UPLOAD_FOLDER+"\\filename"
+        return jsonify(response)
+
+
+
 app.run(host='0.0.0.0', port='5000')
