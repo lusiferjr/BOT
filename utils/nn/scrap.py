50cad79979ed5e243ef978d6fb011d2dfb2d0367
Rohan Rawat <rohan.rawat.9897@gmail.com>
2020-06-30 11:40:54 +0530

    Update scrap.py

--- utils/nn/scrap.py
+++ utils/nn/scrap.py
@@ -1,9 +1,3 @@
-'''
-data plotty
-dataiku
-analytical skills
-
-'''
 from bs4 import BeautifulSoup as bs
 import requests as rq
 USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
